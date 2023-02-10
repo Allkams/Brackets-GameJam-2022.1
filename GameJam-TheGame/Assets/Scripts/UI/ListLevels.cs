5a790a58cd4bfba82b83683fec51b71d744f282a
Allkams <Allkams02@gmail.com>
2023-02-10 14:33:36 +0100

    NewVersion Updated

--- GameJam-TheGame/Assets/Scripts/UI/ListLevels.cs
+++ GameJam-TheGame/Assets/Scripts/UI/ListLevels.cs
@@ -4,33 +4,29 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System;
+using System.Linq;
 
 public class ListLevels : MonoBehaviour
 {
     [SerializeField]
-    private GameObject LevelContainer;
+    private GameObject LevelButton;
     [SerializeField]
-    private MainMenu GameLoadScript;
+    private String[] GameLevels;
 
     private int LevelID;
 
     // Start is called before the first frame update
     void Start()
     {
-        LevelID = SceneManager.sceneCountInBuildSettings - 1;
-        Debug.Log(LevelID);
 
-        for(int i = 0; i < LevelID; i++)
+        for (int i = 0; i < GameLevels.Length; i++)
         {
-           GameObject level =  Instantiate(LevelContainer, new Vector3(this.transform.position.x, this.transform.position.y-(80+62*i)) ,new Quaternion(0,0,0,0),this.transform);
+           GameObject level = Instantiate(LevelButton, new Vector3(this.transform.position.x, this.transform.position.y-(80+62*i)) ,new Quaternion(0,0,0,0),this.transform);
 
-            level.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Level " + (i+1);
+            level.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = GameLevels[i];
             level.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "0/0";
             level.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = "00:00:000";
-
-            level.transform.GetComponent<Button>().onClick.AddListener(() => { Debug.Log("Level:" + i); });
-            level.GetComponent<Button>().onClick.AddListener(() => { Debug.Log("Test"); });
-       
         }
 
         this.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(1088.29f, 100 + (63 * LevelID) + 4);
--- GameJam-TheGame/Assets/Scripts/UI/LoadLevels.cs
+++ GameJam-TheGame/Assets/Scripts/UI/LoadLevels.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoadLevels : MonoBehaviour
 {
-    [SerializeField, Tooltip("The ButtonDesign template")]
-    private Button LevelButtonTemplate;
-
-    private void Awake()
+    public void LoadLevel(string levelName) 
     {
-        
+        SceneManager.LoadScene(levelName);
     }
 }
